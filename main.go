6f212733ea7fd712bd03c44275a5434f5b09f749
auifzysr <auifzysr@gmail.com>
2024-05-06 01:52:34 +0900

    add flags

--- main.go
+++ main.go
@@ -3,26 +3,58 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
 	datatransfer "cloud.google.com/go/bigquery/datatransfer/apiv1"
 	"cloud.google.com/go/bigquery/datatransfer/apiv1/datatransferpb"
+	cli "github.com/urfave/cli/v2"
 )
 
+var region = "asia-northeast1"
+
 func main() {
-	ctx := context.Background()
-	c, err := datatransfer.NewClient(ctx)
-	if err != nil {
-		panic(err)
+	var projectID, transferConfigID string
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "projectID",
+				Aliases:     []string{"p"},
+				Value:       "",
+				Usage:       "projectID",
+				Destination: &projectID,
+			},
+			&cli.StringFlag{
+				Name:        "transferConfigID",
+				Aliases:     []string{"c"},
+				Value:       "",
+				Usage:       "transferConfigID",
+				Destination: &transferConfigID,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			ctx := context.Background()
+			c, err := datatransfer.NewClient(ctx)
+			if err != nil {
+				panic(err)
+			}
+
+			m, err := c.GetTransferConfig(ctx, &datatransferpb.GetTransferConfigRequest{
+				Name: fmt.Sprintf(`projects/%s/locations/%s/transferConfigs/%s`,
+					projectID, region, transferConfigID),
+			})
+
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf("meta: %+v", m)
+			return nil
+		},
 	}
 
-	m, err := c.GetTransferConfig(ctx, &datatransferpb.GetTransferConfigRequest{
-		Name: fmt.Sprintf(`projects/%s/locations/%s/transferConfigs/%s`,
-			projectID, region, transferConfigID),
-	})
-
-	if err != nil {
-		panic(err)
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
-
-	fmt.Printf("meta: %+v", m)
 }
