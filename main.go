deec927915b97b14821a0ca7bd2ec5f3b4be8421
auifzysr <auifzysr@gmail.com>
2024-05-06 22:42:28 +0900

    add list

--- main.go
+++ main.go
@@ -54,6 +54,32 @@ func main() {
 					},
 				},
 			},
+			{
+				Name:    "list",
+				Aliases: []string{"l"},
+				Usage:   "list scheduled query configs",
+				Action: func(cCtx *cli.Context) error {
+					ctx := context.Background()
+					c, err := datatransfer.NewClient(ctx)
+					if err != nil {
+						return fmt.Errorf("data transfer client failed: %w", err)
+					}
+					itr := c.ListTransferConfigs(ctx,
+						&datatransferpb.ListTransferConfigsRequest{
+							Parent: fmt.Sprintf(`projects/%s/locations/%s`, projectID, region),
+						},
+					)
+					for {
+						c, err := itr.Next()
+						if err != nil {
+							fmt.Printf("EOL or failed to iterate response: %s", err)
+							break
+						}
+						fmt.Printf("config: %+v", c)
+					}
+					return nil
+				},
+			},
 		},
 	}
 
