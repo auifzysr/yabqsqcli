76e5a34b39cee07a8d8cb4d928860e46fc000904
auifzysr <auifzysr@gmail.com>
2024-05-06 19:27:11 +0900

    cmd and options

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	datatransfer "cloud.google.com/go/bigquery/datatransfer/apiv1"
 	"cloud.google.com/go/bigquery/datatransfer/apiv1/datatransferpb"
-	cli "github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"
 )
 
 var region = "asia-northeast1"
@@ -25,32 +25,35 @@ func main() {
 				Usage:       "projectID",
 				Destination: &projectID,
 			},
-			&cli.StringFlag{
-				Name:        "transferConfigID",
-				Aliases:     []string{"c"},
-				Value:       "",
-				Usage:       "transferConfigID",
-				Destination: &transferConfigID,
-			},
 		},
-		Action: func(cCtx *cli.Context) error {
-			ctx := context.Background()
-			c, err := datatransfer.NewClient(ctx)
-			if err != nil {
-				panic(err)
-			}
-
-			m, err := c.GetTransferConfig(ctx, &datatransferpb.GetTransferConfigRequest{
-				Name: fmt.Sprintf(`projects/%s/locations/%s/transferConfigs/%s`,
-					projectID, region, transferConfigID),
-			})
-
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Printf("meta: %+v", m)
-			return nil
+		Commands: []*cli.Command{
+			{
+				Name:    "get",
+				Aliases: []string{"g"},
+				Usage:   "get scheduled query config",
+				Action: func(cCtx *cli.Context) error {
+					ctx := context.Background()
+					c, err := datatransfer.NewClient(ctx)
+					if err != nil {
+						return fmt.Errorf("data transfer client failed: %w", err)
+					}
+					m, err := c.GetTransferConfig(ctx, &datatransferpb.GetTransferConfigRequest{Name: fmt.Sprintf(`projects/%s/locations/%s/transferConfigs/%s`, projectID, region, transferConfigID)})
+					if err != nil {
+						return fmt.Errorf("getting transfer failed: %w", err)
+					}
+					fmt.Printf("meta: %+v", m)
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "transferConfigID",
+						Aliases:     []string{"c"},
+						Value:       "",
+						Usage:       "transferConfigID",
+						Destination: &transferConfigID,
+					},
+				},
+			},
 		},
 	}
 
