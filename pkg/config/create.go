e65eca500832597ca676b11a6d71d1919c44c8bc
auifzysr <auifzysr@gmail.com>
2024-06-29 20:27:30 +0900

    options to config

---
+++ pkg/config/create.go
@@ -0,0 +1,30 @@
+package config
+
+type TransferCreateConfig struct {
+	Name               string
+	DisplayName        string
+	DestinationDataset string
+	Query              string
+	Schedule           string
+	Disabled           bool
+
+	// TODO: add options available on google cloud console:
+	// repeatFrequency string
+	// repeatsEvery string
+	// startNow bool
+	// startAtSetTime bool
+	// startDateAndRunTime time.Time
+	// endNever bool
+	// scheduleEndTime time.Time
+	// destinationDatasetID string
+	// destinationTableID string
+	// destinationTablePartitioningField string
+	// destinationTableWritePreference string
+	// automaticLocationSelection bool
+	// locationType string
+	// locationRegion string
+	// serviceAccountEmail string
+	// encryptionKey string
+	// notificationSendEmailNotifications bool
+	// notificationSendCloudPubSubTopic string
+}
