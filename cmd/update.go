c831cbb737bcfd84643ad50725c2c95e8b6951de
auifzysr <auifzysr@gmail.com>
2024-07-01 21:36:06 +0900

    update transfer config

--- cmd/update.go
+++ cmd/update.go
@@ -4,65 +4,27 @@ import (
 	"context"
 	"fmt"
 
-	"cloud.google.com/go/bigquery/datatransfer/apiv1/datatransferpb"
 	"github.com/auifzysr/yabqsqcli/pkg/config"
-	"github.com/auifzysr/yabqsqcli/pkg/domain"
+	"github.com/auifzysr/yabqsqcli/pkg/factory"
 	"github.com/urfave/cli/v2"
-	"google.golang.org/protobuf/types/known/fieldmaskpb"
-	"google.golang.org/protobuf/types/known/structpb"
 )
 
 // TODO: works only with displayName
 func update(cfg *config.UpdateConfig) error {
-	tcs := &domain.TransferConfigsPathSpec{
-		ProjectID: cfg.ProjectID,
-		Location:  cfg.Region,
-		ID:        cfg.ConfigID,
-	}
-	n, err := tcs.Name()
+	tc, err := factory.UpdateTransferConfigFactory(cfg)
 	if err != nil {
 		return err
 	}
-
-	params, err := structpb.NewValue(cfg.Query)
-	if err != nil {
-		return fmt.Errorf("invalid params: %w", err)
-	}
-
-	tc := &datatransferpb.TransferConfig{
-		Name:         n,
-		DisplayName:  cfg.DisplayName,
-		DataSourceId: "scheduled_query",
-		Destination: &datatransferpb.TransferConfig_DestinationDatasetId{
-			DestinationDatasetId: cfg.DestinationDataset,
-		},
-		Params: &structpb.Struct{
-			Fields: map[string]*structpb.Value{
-				"query": params,
-			},
-		},
-		Schedule: cfg.Schedule,
-		Disabled: cfg.Disabled,
-	}
-	// TransferConfig works as proto.Message
-	// TODO: get specified option name and append to the tail
-	fm, err := fieldmaskpb.New(tc, "params")
-	if err != nil {
-		return fmt.Errorf("invalid fieldmask: %w", err)
-	}
 	ctx := context.Background()
-	m, err := client.UpdateTransferConfig(
-		ctx, &datatransferpb.UpdateTransferConfigRequest{
-			TransferConfig: tc,
-			UpdateMask:     fm,
-		},
-	)
+
+	m, err := client.UpdateTransferConfig(ctx, tc)
 	if err != nil {
 		return fmt.Errorf("updating transfer failed: name=%s, err=%w",
 			fmt.Sprintf("projects/%s/locations/%s/transferConfigs/%s",
 				cfg.ProjectID, cfg.Region, cfg.ConfigID), err)
 	}
 	fmt.Printf("meta: %+v", m)
+
 	return nil
 }
 
