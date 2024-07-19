bdb94562b73e3f7f1ff45eec66b51ba4ada0e49a
auifzysr <auifzysr@gmail.com>
2024-07-19 11:44:03 +0900

    append fields to transfer config

--- pkg/factory/update.go
+++ pkg/factory/update.go
@@ -21,29 +21,29 @@ func UpdateTransferConfigFactory(cfg *config.UpdateConfig) (*datatransferpb.Upda
 	if err != nil {
 		return nil, err
 	}
-	params, err := structpb.NewValue(cfg.Query)
-	if err != nil {
-		return nil, fmt.Errorf("invalid params: %w", err)
-	}
 
 	tc := &datatransferpb.TransferConfig{
-		Name:         n,
-		DisplayName:  cfg.DisplayName,
-		DataSourceId: "scheduled_query",
-		Destination: &datatransferpb.TransferConfig_DestinationDatasetId{
-			DestinationDatasetId: cfg.DestinationDatasetID,
-		},
-		Params: &structpb.Struct{
+		Name: n,
+	}
+
+	var fieldMaskPaths []string
+
+	if cfg.Query != "" {
+		params, err := structpb.NewValue(cfg.Query)
+		if err != nil {
+			return nil, fmt.Errorf("invalid params: %w", err)
+		}
+		tc.Params = &structpb.Struct{
 			Fields: map[string]*structpb.Value{
 				"query": params,
 			},
-		},
-		Schedule: cfg.Schedule,
-		Disabled: cfg.Disabled,
+		}
+
+		fieldMaskPaths = append(fieldMaskPaths, "params")
 	}
+
 	// TransferConfig works as proto.Message
-	// TODO: get specified option name and append to the tail
-	fm, err := fieldmaskpb.New(tc, "params")
+	fm, err := fieldmaskpb.New(tc, fieldMaskPaths...)
 	if err != nil {
 		return nil, fmt.Errorf("invalid fieldmask: %w", err)
 	}
