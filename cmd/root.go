aabba9b6762b793d72f06b9c35ceec62e4d76352
auifzysr <auifzysr@gmail.com>
2024-07-25 15:51:41 +0900

    new file

--- cmd/root.go
+++ cmd/root.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"os"
 
-	"cloud.google.com/go/bigquery/datatransfer/apiv1/datatransferpb"
 	"github.com/auifzysr/yabqsqcli/pkg/config"
 	"github.com/auifzysr/yabqsqcli/pkg/domain"
-	"github.com/auifzysr/yabqsqcli/pkg/factory"
 	"github.com/urfave/cli/v2"
 )
 
@@ -67,22 +65,4 @@ func Run() error {
 	}
 
 	return app.Run(os.Args)
-}
-
-func callList(ctx context.Context, cfg *config.ListConfig) ([]*datatransferpb.TransferConfig, error) {
-	tc, err := factory.ListTransferConfigFactory(cfg)
-	if err != nil {
-		return nil, err
-	}
-	var res []*datatransferpb.TransferConfig
-	itr := client.ListTransferConfigs(ctx, tc)
-	for {
-		m, err := itr.Next()
-		if err != nil {
-			break
-		}
-		res = append(res, m)
-	}
-
-	return res, nil
 }
